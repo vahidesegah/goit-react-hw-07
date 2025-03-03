e96184b1973c7f5e2bed0665ebc3ea53bac6da0d
Vahide Segah Üney <vahidesegah@gmail.com>
2025-03-03 20:08:13 +0300

    init

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,27 +1,27 @@
-import { useDispatch, useSelector } from 'react-redux';
-import { changeFilter, selectNameFilter } from '../../redux/filtersSlice';
-import css from './Filter.module.css';
+import { useDispatch, useSelector } from "react-redux";
+import { changeFilter, selectNameFilter } from "../../redux/filtersSlice";
+import css from "./Filter.module.css";
 
 const Filter = () => {
-    const dispatch = useDispatch();
-    const filter = useSelector(selectNameFilter);
+  const dispatch = useDispatch();
+  const filter = useSelector(selectNameFilter);
 
-    return (
-        <div className={css.filterContainer}>
-            <div className={css.filterWrapper}>
-                <label className={css.label}>
-                    Find contacts by name
-                    <input
-                        className={css.input}
-                        type="text"
-                        value={filter}
-                        onChange={e => dispatch(changeFilter(e.target.value))}
-                        placeholder="Search contacts..."
-                    />
-                </label>
-            </div>
-        </div>
-    );
+  return (
+    <div className={css.filterContainer}>
+      <div className={css.filterWrapper}>
+        <label className={css.label}>
+          Find Contacts by Name
+          <input
+            className={css.input}
+            type="text"
+            value={filter}
+            onChange={(e) => dispatch(changeFilter(e.target.value))}
+            placeholder="Search contacts..."
+          />
+        </label>
+      </div>
+    </div>
+  );
 };
 
-export default Filter; 
+export default Filter;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,10 @@
-import { configureStore } from '@reduxjs/toolkit';
-import { contactsReducer } from './contactsSlice';
-import { filtersReducer } from './filtersSlice';
+import { configureStore } from "@reduxjs/toolkit";
+import { contactsReducer } from "./contactsSlice";
+import { filtersReducer } from "./filtersSlice";
 
 export const store = configureStore({
-    reducer: {
-        contacts: contactsReducer,
-        filters: filtersReducer
-    }
-}); 
+  reducer: {
+    contacts: contactsReducer,
+    filters: filtersReducer,
+  },
+});
